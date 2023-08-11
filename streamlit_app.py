e09b905a05b4cb29f9473efaa03dba9fd61e5e4a
sk9020 <142035649+sk9020@users.noreply.github.com>
2023-08-11 21:43:59 +0530

    Update streamlit_app.py
    added more content

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit
 streamlit.title('Sugreev Snowflake Badge 2 course')
 streamlit.header('Learning app building with streamlit')
 streamlit.text('Badge 2')
+
+streamlit.title('Food')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
