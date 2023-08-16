f72c86b04c5c75a864b162e8ccad8e0a296d75af
sk9020 <142035649+sk9020@users.noreply.github.com>
2023-08-16 14:47:12 +0530

    Update streamlit_app.py to get fruityvice api data

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,8 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show =  my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+# New section to display fruityVice api
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
