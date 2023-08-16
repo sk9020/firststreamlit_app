6eac5b8979c498adf244ec0d15b626668954ad16
sk9020 <142035649+sk9020@users.noreply.github.com>
2023-08-16 14:58:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ fruits_to_show =  my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityVice api
+streamlit.text('New section to display fruityVice api')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
