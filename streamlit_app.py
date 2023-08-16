b3fcfc748aa19bd48b24a74437002dca940b590e
sk9020 <142035649+sk9020@users.noreply.github.com>
2023-08-16 18:29:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ try:
   else:
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         # streamlit.text(fruityvice_response.json())
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       streamlit.dataframe(fruityvice_normalized)
    
 except URLError as e:
  streamlit.error()
